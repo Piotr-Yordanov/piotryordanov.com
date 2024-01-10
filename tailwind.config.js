db04fe937cb1884aef4eb5b5fd29abd0b4fea619
Alpha <piotr@piotryordanov.com>
2024-01-10 11:34:00 +0100

    fix: dep management to be able to deploy on vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -155,8 +155,5 @@ module.exports = {
       }),
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-  ],
+  plugins: [require('@tailwindcss/typography')],
 };
