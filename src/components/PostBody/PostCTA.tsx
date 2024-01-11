e79ef085392c3845a7868adf9d55cde51eabbc79
Alpha <piotr@piotryordanov.com>
2024-01-11 20:00:54 +0100

    feat: update post CTA content

--- src/components/PostBody/PostCTA.tsx
+++ src/components/PostBody/PostCTA.tsx
@@ -4,19 +4,35 @@ export default function PostCTA({ children }: Props) {
   return (
     <div className="mt-20 rounded-lg border-2 bg-white p-8 font-lato">
       <div className="text-xl font-bold">
-        Whenever you're ready, there are 4 ways I can help you:
+        Whenever you're ready, there are 3 ways I can help you:
       </div>
-      <ul className="list-decimal text-lg">
+      <ul className="mt-4 list-decimal pl-8 text-lg">
         <li>
+          Subscribe to my
           <CustomLink
-            href="https://google.com"
-            className="text-blue-500 hover:underline"
+            href="/newsletter"
+            className="ml-1 text-primary hover:underline"
           >
-            Now this is a story all about how, my life got flipped-turned upside
+            Newsletter
           </CustomLink>
         </li>
         <li>
-          Now this is a story all about how, my life got flipped-turned upside
+          <CustomLink
+            href="https://calendly.com/piotry/hello"
+            className="mr-1 text-primary hover:underline"
+          >
+            Hire me
+          </CustomLink>
+          to help you solve your quantitative trading challenges.
+        </li>
+        <li>
+          Check out my trading
+          <CustomLink
+            href="/trading"
+            className="ml-1 text-primary hover:underline"
+          >
+            products
+          </CustomLink>
         </li>
       </ul>
     </div>
